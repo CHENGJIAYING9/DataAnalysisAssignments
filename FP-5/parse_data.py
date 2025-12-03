95bbcacc7dbaf86d32a4e591e3ded7c7975338c7
CHENGJIAYING9 <isabellacheng32@hotmail.com>
2025-12-03 21:53:46 +0800

    Delete FP-5 directory

--- FP-5/parse_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def parse_data():
-    df0 = pd.read_csv('student-mat.csv', sep = ';')
-    df = df0[  ['studytime', 'absences', 'health', 'Medu', 'Fedu', 'G3']  ]
-    df = df.rename( columns={'Medu':'medu', 'Fedu':'fedu', 'G3':'grade'} )
-    return df
-
-
